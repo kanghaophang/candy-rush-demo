cc00fc08c6d35c2376665be49833707bdc520f74
kanghaophang <haophang360@gmail.com>
2025-11-07 02:49:20 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: When deploying to GitHub Pages, set `base` to '/<your-repo-name>/'.
-// Option A: temporarily edit `base` below before `npm run build`.
-// Option B: run `REPO_NAME=<your-repo> npm run build:gh` which overrides base at build time.
 export default defineConfig({
   plugins: [react()],
-  // base: '/your-repo-name/',
+  base: '/candy-rush-demo/',   // 👈 这里填你的仓库名
 })
